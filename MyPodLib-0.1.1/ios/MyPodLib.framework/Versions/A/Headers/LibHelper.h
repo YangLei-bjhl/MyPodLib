71fd094036e2a841cb800d445d50876588468ce5
YangLei-bjhl <jxyl9010@gmail.com>
2015-05-12 20:08:09 +0800

    0.1.1

---
+++ MyPodLib-0.1.1/ios/MyPodLib.framework/Versions/A/Headers/LibHelper.h
@@ -0,0 +1,17 @@
+//
+//  LibHelper.h
+//  Pods
+//
+//  Created by 杨磊 on 15/5/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ *  123
+ */
+@interface LibHelper : NSObject
+
++ (void)libLog:(NSString *)log;
+@end
